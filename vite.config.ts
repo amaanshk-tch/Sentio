52de87acf71ad78755d93685c6ea8ad2e81680f9
Amaan <amaanullah1605@gmail.com>
2026-04-22 16:34:41 +0530

    code clean

--- vite.config.ts
+++ vite.config.ts
@@ -31,13 +31,11 @@ export default defineConfig(({ mode }) => ({
     dedupe: ["react", "react-dom", "react/jsx-runtime", "react/jsx-dev-runtime", "@tanstack/react-query", "@tanstack/query-core"],
   },
   build: {
+    chunkSizeWarningLimit: 1000,
     rollupOptions: {
       output: {
         manualChunks(id) {
           if (id.includes("node_modules")) {
-            if (id.includes("react")) {
-              return "vendor-react";
-            }
             return "vendor";
           }
         },
